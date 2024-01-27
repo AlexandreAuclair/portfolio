3d23e0f8f480e19c54a7175c6504ddce672ded63
alex <alexandre26auclair@gmail.com>
2024-01-27 18:57:10 -0500

    Navbar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home"
+
+import {Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </>
   );
 }
 
