9191c3d31cee5f3d8381abc0879c143723c5470a
alex <alexandre26auclair@gmail.com>
2024-01-28 15:26:48 -0500

    hero et navbar

---
+++ react-portfolio/src/components/Hero.js
@@ -0,0 +1,20 @@
+import "./HeroStyles.css";
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <h2>// Hello, World!</h2>
+        <span className="mask">
+            <p>Mon nom est Alexandre Auclair et je suis présentement en baccalauréat en informatique à l'université de sherbrooke.</p>
+            <p>J'ai construit ce site web pour cumulé tout mes projet que j'ai fait dans ma vie en rapport avec l'informatique.</p>
+            <p>Pusique que j'ai une technique en intégration multimédia j'ai beaucoup travailler dans le web, mais je suis intéressé 
+                par beaucoup de sujet lié à l'informatique en général. </p>
+            <p>Ma grande passion c'est les jeux vidéo donc tous ce tourne autour de cela est un plus pour moi.</p>
+        </span>
+    </div>
+  )
+}
+
+export default Hero
